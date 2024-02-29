54d7c3fac6104223ff53b2adef27a3430d25dc43
DavideCiammetti <davideciammetti97@gmail.com>
2024-02-29 17:05:44 +0100

    feat: scaffolding

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script>
+</script>
+
+<template>
+  <div>
+    <h1 class="text-danger">ciao</h1>
+    <font-awesome-icon icon="fa-solid fa-house" />
+    <router-link></router-link>
+  </div>
+</template>
+
+<style>
+</style>
---
+++ src/main.js
@@ -0,0 +1,19 @@
+import { createApp } from 'vue'
+// scss
+import './style/style.scss'
+// bootstrap
+import * as bootstrap from 'bootstrap'
+// axios
+import axios from 'axios'
+// router
+import {router} from './router'
+import App from './App.vue'
+// importazione icon components
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+// importazione libreria fontawesome
+import { library } from "@fortawesome/fontawesome-svg-core";
+// icone
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
+library.add(faHouse)
+
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).use(router).mount('#app')
