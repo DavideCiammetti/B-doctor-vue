0887f7f8451490f13fc871a99aefaf572f3a7c73
MatteoSanson <matteo.sanson@gmail.com>
2024-03-06 22:49:45 +0100

    feat: components footer

--- src/components/footer/DownFooter.vue
+++ src/components/footer/DownFooter.vue
@@ -5,9 +5,28 @@ export default {
 </script>
 
 <template>
-    <div>down footer</div>
+    <div class="hr-container">
+        <hr class="hr-line">
+    </div>
+    <div class="col-12 col-md-8 mx-auto text-center mt-3">
+        <span>Copyright © 2024 Doctolib, tutti i diritti riservati.</span>
+    </div>
 </template>
 
 <style scoped lang="scss">
 @use'../../style/partials/palette.scss' as *;
+
+.hr-container {
+    display: flex;
+    justify-content: center;
+}
+
+.hr-line {
+    width: 100%; // Lunghezza iniziale per mobile
+
+    @media (min-width: 992px) {
+        // Desktop
+        width: 90%;
+    }
+}
 </style>
--- src/components/footer/UpFooter.vue
+++ src/components/footer/UpFooter.vue
@@ -5,23 +5,26 @@ export default {
 </script>
 
 <template>
-    <div class="row text-center">
-        <!-- Titolo -->
-        <div class="col-12 col-lg-4">
-            <h1 class="mb-3 mb-lg-0 txt-cstm">Domande? Hai bisogno di aiuto?</h1>
-        </div>
-        <!-- Paragrafo -->
-        <div class="col-12 col-lg-4 txt-cstm">
-            <p class="mb-lg-0">Vai agli articoli del centro d'assistenza o contattaci.</p>
-        </div>
-        <!-- Bottone -->
-        <div class="col-12 col-lg-4">
-            <button class="btn btn-primary btn-lg btn-cstm">
-                <div class="d-flex gap-3 align-items-center">
-                    <font-awesome-icon icon="fa-solid fa-circle-info" />
-                    <span>VAI AL CENTRO D'ASSISTENZA</span>
-                </div>
-            </button>
+    <div class="container-fluid">
+        <div class="row text-center">
+            <!-- Sezione sinistra -->
+            <div class="col-12 col-lg-6 d-flex flex-column align-items-center">
+                <!-- Titolo -->
+                <h1 class="mb-3 txt-cstm text-left">Domande? Hai bisogno di aiuto?</h1>
+                <!-- Paragrafo -->
+                <p class="mb-3 text-left">Vai agli articoli del centro d'assistenza o contattaci.</p>
+            </div>
+            <!-- Sezione destra -->
+            <div
+                class="col-12 col-lg-6 d-flex justify-content-center justify-content-lg-end align-items-center mt-3 mt-lg-0">
+                <!-- Bottone -->
+                <button class="btn btn-primary btn-lg btn-cstm">
+                    <div class="d-flex gap-3 align-items-center">
+                        <font-awesome-icon icon="fa-solid fa-circle-info" />
+                        <span>VAI AL CENTRO D'ASSISTENZA</span>
+                    </div>
+                </button>
+            </div>
         </div>
     </div>
 </template>
@@ -29,15 +32,36 @@ export default {
 <style scoped lang="scss">
 @use '../../style/partials/palette.scss' as *;
 
-div.col-12 {
+.container-fluid {
     h1.txt-cstm {
         font-size: 2rem;
     }
+
+    p {
+        font-size: 1.375rem;
+    }
+
+    // Mobile
+    @media (min-width: 576px) {
+        width: 90%; // Tablet
+    }
+
+    // Desktop
+    @media (min-width: 992px) {
+        .col-12.col-lg-6.d-flex.flex-column.justify-content-center {
+            flex-direction: row;
+            align-items: center;
+        }
+    }
 }
 
-// bottone
 .btn-cstm {
     border: none;
     background-color: $green-700;
+
+    // Desktop
+    @media (min-width: 992px) {
+        width: auto;
+    }
 }
 </style>
