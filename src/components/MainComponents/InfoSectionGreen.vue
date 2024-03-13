a6d15ca767b82090325d394971121c42ef03ffed
MatteoSanson <matteo.sanson@gmail.com>
2024-03-13 12:42:42 +0100

    feat: link to laravel home

--- src/components/MainComponents/InfoSectionGreen.vue
+++ src/components/MainComponents/InfoSectionGreen.vue
@@ -25,7 +25,7 @@ export default {
                     </div>
                     <div>
                         <p class="text-white fs-14">
-                            Guadagna tempo grazie alla prenotazione online e 
+                            Guadagna tempo grazie alla prenotazione online e
                             migliora il coordinamento con la tua segreteria
                             condividendo note in tempo reale.
                         </p>
@@ -38,9 +38,9 @@ export default {
                     </div>
                     <div>
                         <p class="text-white fs-14">
-                           Migliora la tua giornata lavorativa, organizzando al meglio
-                           i tuoi task e comunicando in maniera sicura e automatica
-                           con i tuoi pazienti.
+                            Migliora la tua giornata lavorativa, organizzando al meglio
+                            i tuoi task e comunicando in maniera sicura e automatica
+                            con i tuoi pazienti.
                         </p>
                     </div>
                 </li>
@@ -51,10 +51,10 @@ export default {
                     </div>
                     <div>
                         <p class="text-white fs-14">
-                           Proponi ai tuoi pazienti un servizio all'avanguardia,
-                           offrendo la possibilità di prenotare online anche fuori 
-                           dagli orari di segreteria e di gestire autonomamente la loro 
-                           salute e quella dei loro cari.
+                            Proponi ai tuoi pazienti un servizio all'avanguardia,
+                            offrendo la possibilità di prenotare online anche fuori
+                            dagli orari di segreteria e di gestire autonomamente la loro
+                            salute e quella dei loro cari.
                         </p>
                     </div>
                 </li>
@@ -65,51 +65,56 @@ export default {
                     </div>
                     <div>
                         <p class="text-white fs-14">
-                           Migliora la tua visibilità online e riduci fino al 70% i no-show.
+                            Migliora la tua visibilità online e riduci fino al 70% i no-show.
                         </p>
                     </div>
                 </li>
             </ul>
             <div class="ms-4">
-                <button class="button-pro fw-medium">
+                <a class="btn button-pro fw-medium" href="http://127.0.0.1:8000" target="_blank">
                     SCOPRI BDOCTORS PRO
-                </button>
+                </a>
             </div>
         </div>
     </div>
 </template>
 
 <style scoped lang="scss">
-    @use'../../style/partials/palette.scss' as *;
+@use'../../style/partials/palette.scss' as *;
 
-    ul{
-        list-style: none;
-    }
-    /*
+ul {
+    list-style: none;
+}
+
+/*
         UTILITY
-    */ 
-    // larghezza container
-    .width-percent{
-        width: 90%;
-        padding: 1.5625rem;
-    }
-    .mt-90{
-        margin-top: 5.625rem;
-    }
-    .bg-col-green{
-        background-color: $green-600;
-        border-radius: 1.25rem;
-    }
-    // font paragrafi
-    .fs-14{
-        font-size: .875rem;
-    }
-    .button-pro{
-        border: none;
-        color: white;
-        background-color: #5eeace;
-        padding: .625rem;
-        border-radius: .625rem;
-        font-size: .875rem;
-    }
+    */
+// larghezza container
+.width-percent {
+    width: 90%;
+    padding: 1.5625rem;
+}
+
+.mt-90 {
+    margin-top: 5.625rem;
+}
+
+.bg-col-green {
+    background-color: $green-600;
+    border-radius: 1.25rem;
+}
+
+// font paragrafi
+.fs-14 {
+    font-size: .875rem;
+}
+
+.button-pro {
+    border: none;
+    color: white;
+    background-color: #5eeace;
+    padding: .625rem;
+    border-radius: .625rem;
+    font-size: .875rem;
+}
 </style>
